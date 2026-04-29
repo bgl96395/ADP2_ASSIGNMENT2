da129d0474f7a43fadb624bb8f7721c7e35370da
bigali <bigalinugmas7@gmail.com>
2026-04-29 18:57:42 +0500

    zz

---
+++ notification-service/cmd/notificationService'/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bgl96395/ADP2_ASSIGNMENT2/notification-service/internal/subscriber"
+)
+
+func main() {
+	natsURL := os.Getenv("NATS_URL")
+	if natsURL == "" {
+		natsURL = "nats://localhost:4222"
+	}
+
+	sub, err := subscriber.Connect(natsURL)
+	if err != nil {
+		log.Fatalf("ERROR: failed to connect to NATS after retries: %v", err)
+	}
+
+	if err := sub.Subscribe(); err != nil {
+		log.Fatalf("ERROR: failed to subscribe: %v", err)
+	}
+
+	log.Println("Notification Service is running. Waiting for events...")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down Notification Service...")
+	sub.Drain()
+	log.Println("Notification Service stopped cleanly")
+	os.Exit(0)
+}
